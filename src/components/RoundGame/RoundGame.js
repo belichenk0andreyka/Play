c3613b2d3fb46efccc520ba74a074d7cae2c9b60
AndreyBelichenko <andreybe@wizardsdev.com>
2021-08-12 22:56:18 +0300

    make position of image

--- src/components/RoundGame/RoundGame.js
+++ src/components/RoundGame/RoundGame.js
@@ -1,19 +1,30 @@
 import * as React from 'react';
 
+import './RoundGame.less';
+
 const RoundGame = ({data}) => {
+    const getPositionOfImg = (rawSpan, tdIndex) => {
+        if (rawSpan > 1 && tdIndex <= 1) {
+            return 'td_img__bottom';
+        } else if (rawSpan > 1 && tdIndex >= 2) {
+            return 'td_img__top';
+        }
+    };
     return (
         <table border="1" align="center" className='table'>
-            {data.map((gameTr, trIndex) => (
-                <tr className='table_tr' key={trIndex}>
-                    {gameTr.map((gameTd, tdIndex) => gameTd.value && (
-                        <td rowSpan={gameTd.rawSpan} className='table_td' key={tdIndex}>
-                            <div className='td_content'>
-                                <img src={`../../images/${gameTd.value}.png`}/>
-                            </div>
-                        </td>
-                    ))}
-                </tr>
-            ))}
+            <tbody>
+                {data.map((gameTr, trIndex) => (
+                    <tr className='table_tr' key={trIndex}>
+                        {gameTr.map((gameTd, tdIndex) => gameTd.value && (
+                            <td rowSpan={gameTd.rawSpan} className='table_td' key={tdIndex}>
+                                <div className={`td_content ${gameTd.rawSpan > 1 ? 'td_content__background' : ''}`}>
+                                    <img src={`../../images/${gameTd.value}.png`} className={getPositionOfImg(gameTd.rawSpan, tdIndex)} />
+                                </div>
+                            </td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
         </table>
     );
 };
